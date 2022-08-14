3ddd01f1bc6f6b9941f17c2d8c0a071a184132d3
Noha Mohamed <nohamohamedali1993@gmail.com>
2022-08-15 01:42:31 +0200

    First Commit

--- Tests/CharchtersCountViewTests/CharchtersCountViewTests.swift
+++ Tests/CharchtersCountViewTests/CharchtersCountViewTests.swift
@@ -6,6 +6,5 @@ final class CharchtersCountViewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CharchtersCountView().text, "Hello, World!")
     }
 }
