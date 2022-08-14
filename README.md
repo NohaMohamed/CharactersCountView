546b2472fde9be5e850a5da838662040e9ec834a
Noha Mohamed <nohamohamedali1993@gmail.com>
2022-08-15 01:39:52 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CharchtersCountView
+
+A description of this package.
---
+++ Sources/CharchtersCountView/CharchtersCountView.swift
@@ -0,0 +1,6 @@
+public struct CharchtersCountView {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
